c0bfd37af5854433367f18cf48e52842556db837
Corneliu Rusu <snipklas@gmail.com>
2023-10-18 12:11:31 +0200

    Variables to the Path/removing hardcoded path

--- src/Service/UploadHelper.php
+++ src/Service/UploadHelper.php
@@ -20,11 +20,15 @@ class UploadHelper
 
     private $logger;
 
-    public function __construct(FilesystemInterface $publicUploadsFilesystem, RequestStackContext $requestStackContext, LoggerInterface $logger)
+    private $publicAssetBaseUrl;
+
+
+    public function __construct(FilesystemInterface $publicUploadsFilesystem, RequestStackContext $requestStackContext, LoggerInterface $logger, string $uploadedAssetsBaseUrl)
     {
         $this->filesystem = $publicUploadsFilesystem;
         $this->requestStackContext = $requestStackContext;
         $this->logger = $logger;
+        $this->publicAssetBaseUrl = $uploadedAssetsBaseUrl;
     }
 
     public function uploadArticleImage(File $file, ?string $existingFilename): string
@@ -69,6 +73,6 @@ class UploadHelper
     {
         // needed if you deploy under a subdirectory
         return $this->requestStackContext
-            ->getBasePath() . '/uploads/' . $path;
+            ->getBasePath() . $this->publicAssetBaseUrl . '/' . $path;
     }
 }
