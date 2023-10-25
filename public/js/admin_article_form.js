7ecf3bfc180089bd43d71ea88016a5ff78627e1f
Corneliu Rusu <snipklas@gmail.com>
2023-10-25 14:28:49 +0200

    Rendering references for article

--- public/js/admin_article_form.js
+++ public/js/admin_article_form.js
@@ -1,7 +1,9 @@
 Dropzone.autoDiscover = false;
 
 $(document).ready(function () {
-  initializeDropzone();
+  var referenceList = new ReferenceList($(".js-reference-list"));
+
+  initializeDropzone(referenceList);
 
   var $locationSelect = $(".js-article-form-location");
   var $specificLocationTarget = $(".js-specific-location-target");
@@ -27,7 +29,47 @@ $(document).ready(function () {
   });
 });
 
-function initializeDropzone() {
+// todo - use Webpack Encore so ES6 syntax is transpiled to ES5
+class ReferenceList {
+  constructor($element) {
+    this.$element = $element;
+    this.references = [];
+    this.render();
+
+    $.ajax({
+      url: this.$element.data("url"),
+    }).then((data) => {
+      this.references = data;
+      this.render();
+    });
+  }
+
+  addReference(reference) {
+    this.references.push(reference);
+    this.render();
+  }
+
+  render() {
+    const itemsHtml = this.references.map((reference) => {
+      return `
+<li class="list-group-item d-flex justify-content-between align-items-center">
+    ${reference.originalFilename}
+
+    <span>
+        <a href="/admin/article/references/${reference.id}/download"><span class="fa fa-download"></span></a>
+    </span>
+</li>
+`;
+    });
+
+    this.$element.html(itemsHtml.join(""));
+  }
+}
+
+/**
+ * @param {ReferenceList} referenceList
+ */
+function initializeDropzone(referenceList) {
   var formElement = document.querySelector(".js-reference-dropzone");
   if (!formElement) {
     return;
@@ -36,10 +78,8 @@ function initializeDropzone() {
   var dropzone = new Dropzone(formElement, {
     paramName: "reference",
     init: function () {
-      this.on("error", function (file, data) {
-        if (data.detail) {
-          this.emit("error", file, data.detail);
-        }
+      this.on("success", function (file, data) {
+        referenceList.addReference(data);
       });
     },
   });
